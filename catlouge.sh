5217a0c590a2f1e2a78cae2f5c1bfeb332e0c931
sivakumar <sivakumarthalla123@gmail.com>
2026-02-01 15:47:25 +0530

    variables

--- catlouge.sh
+++ catlouge.sh
@@ -4,6 +4,7 @@ user=$(id -u)
 log_folder="/var/log/catalouge-logs"
 log_file="/var/log/catalouge-logs/$0.log"
 SCRIPT_DIR=$PWD
+$MONGODB_HOST="mongodb.sivadevops707.online"
 if [ $user -ne 0 ]; then
     echo "please run as sudo user / root user" | tee -a $log_file
    exit 1
@@ -34,8 +35,10 @@ if [ $? -ne 0 ]; then
 else
     echo -"Roboshop user already exist ...  SKIPPING"
 fi
-mkdir /app 
+mkdir -p /app 
 validate $? "creating app directory"
+rm -rf /app/*
+VALIDATE $? "Removing existing code"
 
 curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue-v3.zip &>>$log_file
 validate $? "downloading code from git"
@@ -56,3 +59,18 @@ validate $? "daemon reload ..."
 systemctl enable catalogue &>>$log_file
 systemctl start catalogue &>>$log_file
 validate $? "start catalogue..."
+
+cp $SCRIPT_DIR/mongo.repo /etc/yum.repos.d/mongo.repo
+dnf install mongodb-mongosh -y &>>$LOGS_FILE
+
+INDEX=$(mongosh --host $MONGODB_HOST --quiet  --eval 'db.getMongo().getDBNames().indexOf("catalogue")')
+
+if [ $INDEX -le 0 ]; then
+    mongosh --host $MONGODB_HOST </app/db/master-data.js
+    VALIDATE $? "Loading products"
+else
+    echo -e "Products already loaded ... $Y SKIPPING $N"
+fi
+
+systemctl restart catalogue
+VALIDATE $? "Restarting catalogue"
