97d536f73c92cd10b065899760671ac20eb2d781
sivakumar <sivakumarthalla123@gmail.com>
2026-02-01 17:45:25 +0530

    variables

--- frontend.sh
+++ frontend.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 user=$(id -u)
-log_folder="/var/log/catalouge-logs"
-log_file="/var/log/catalouge-logs/$0.log"
+log_folder="/var/log/fend-logs"
+log_file="/var/log/fend-logs/$0.log"
 SCRIPT_DIR=$PWD
 MONGODB_HOST="mongodb.sivadevops707.online"
 if [ $user -ne 0 ]; then
@@ -21,27 +21,28 @@ validate(){
     fi
 }
 
-dnf module disable nginx -y
+dnf module disable nginx -y &>>$log_file
 validate $? "disable nginx"
-dnf module enable nginx:1.24 -y
+dnf module enable nginx:1.24 -y &>>$log_file
 validate $? "enable nginx"
-dnf install nginx -y
+dnf install nginx -y &>>$log_file
 validate $? "install nginx"
 
-systemctl enable nginx 
-systemctl start nginx 
+systemctl enable nginx &>>$log_file 
+systemctl start nginx &>>$log_file
 validate $? "enable and start nginx"
 
-rm -rf /usr/share/nginx/html/* 
+rm -rf /usr/share/nginx/html/*  &>>$log_file
 validate $? "removing nginx html"
-cd /usr/share/nginx/html 
+cd /usr/share/nginx/html &>>$log_file
 validate $? "changing folder nginx html"
-
-unzip /tmp/frontend.zip
+curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend-v3.zip &>>$log_file
+validate $? "downloaded zip file..."
+unzip /tmp/frontend.zip &>>$log_file
 validate $? "unzip nginx html"
 
-cp $SCRIPT_DIR/frontend.conf /etc/nginx/nginx.conf
+cp $SCRIPT_DIR/frontend.conf /etc/nginx/nginx.conf &>>$log_file
 validate $? "frontend configuration ..."
 
-systemctl restart nginx 
+systemctl restart nginx  &>>$log_file
 validate $? "nginx restarted......"
