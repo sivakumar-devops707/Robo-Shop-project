9e5e8963ce033994519a28c5d42f316b35f3bd1a
sivakumar <sivakumarthalla123@gmail.com>
2026-02-01 19:41:32 +0530

    variables

---
+++ mysql.sh
@@ -0,0 +1,29 @@
+user=$(id -u)
+log_folder="/var/log/mysql-logs"
+log_file="/var/log/mysql-logs/$0.log"
+
+if [ $user -ne 0 ]; then
+    echo "please run as sudo user / root user" | tee -a $log_file
+   exit 1
+
+fi
+mkdir -p $log_folder
+validate(){
+    if [ $1 -ne 0 ]; then
+      echo "installing $2 failure...." | tee -a $log_file
+      exit 1
+    else
+
+      echo "installing  $2  success" | tee -a $log_file
+    fi
+}
+
+dnf install mysql-server -y
+validate $? "install mysql-server..."
+
+systemctl enable mysqld
+systemctl start mysqld  
+
+validate $? "enable and start mysql.."
+mysql_secure_installation --set-root-pass RoboShop@1
+validate $? "set root password..."
