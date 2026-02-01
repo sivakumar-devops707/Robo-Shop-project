f5b939b0064f4f6dbc280f6270e4f99c30ab1e42
sivakumar <sivakumarthalla123@gmail.com>
2026-02-01 18:25:18 +0530

    variables

---
+++ user.sh
@@ -0,0 +1,63 @@
+#!/bin/bash
+
+user=$(id -u)
+log_folder="/var/log/user-logs"
+log_file="/var/log/user-logs/$0.log"
+SCRIPT_DIR=$PWD
+REDIS_HOST="redis.sivadevops707.online"
+if [ $user -ne 0 ]; then
+    echo "please run as sudo user / root user" | tee -a $log_file
+   exit 1
+
+fi
+mkdir -p $log_folder
+validate(){
+    if [ $1 -ne 0 ]; then
+      echo "installing $2 failure...." | tee -a $log_file
+      exit 1
+    else
+
+      echo "installing  $2  success" | tee -a $log_file
+    fi
+}
+
+dnf module disable nodejs -y &>>$log_file
+dnf module enable nodejs:20 -y &>>$log_file
+validate $? "enable nodejs"
+
+dnf install nodejs -y &>>$log_file
+validate $? "install nodejs"
+
+id roboshop &>>$log_file
+echo $?
+if [ $? -ne 0 ]; then
+     sudo useradd --system --home /app --shell /sbin/nologin --comment "roboshop system user" roboshop &>>$log_file
+    validate $? "Creating system user"
+else
+    echo "Roboshop user already exist ...  SKIPPING"
+fi
+mkdir -p /app 
+validate $? "creating app directory"
+rm -rf /app/*
+
+validate $? "Removing existing code"
+
+curl -L -o /tmp/user.zip https://roboshop-artifacts.s3.amazonaws.com/user-v3.zip &>>$log_file
+validate $? "downloading code from git"
+cd /app 
+validate $? "change directory"
+unzip /tmp/user.zip &>>$log_file
+validate $? "unzip code.."
+cd /app 
+npm install &>>$log_file
+validate $? "npm install.."
+
+cp $SCRIPT_DIR/user.service /etc/systemd/system/user.service &>>$log_file
+validate $? "copying user service..."
+
+systemctl daemon-reload &>>$log_file
+validate $? "daemon reload ..."
+
+systemctl enable user &>>$log_file
+systemctl start user &>>$log_file
+validate $? "start user..."
